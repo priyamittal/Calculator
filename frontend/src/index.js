861f887b01f036aba0bf6a10bf5e45baf767b1f2
Priya Mittal <priya.mittal1@tcs.com>
2018-11-21 19:33:38 +0100

    Create basic infrastructure for frontend app

---
+++ frontend/src/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const title = 'Basic setup';
+
+ReactDOM.render(
+<div>{title}</div>,
+document.getElementById('app')
+);
---
+++ frontend/webpack.config.js
@@ -0,0 +1,39 @@
+const path = require('path')
+const webpack = require('webpack')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const PATHS = {
+    src: path.join(__dirname, 'src'),
+    dist: path.join(__dirname, 'dist'),
+}
+
+module.exports = {
+    entry: './src/index.js',
+    module: {
+        rules: [
+            {
+                test: /\.(js|jsx)$/,
+                exclude: /node_modules/,
+                use: ['babel-loader']
+            }
+        ]
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            filename: 'index.html',
+            chunks: ['index'],
+            template: path.join(PATHS.src, 'index.html'),
+        }),
+        new webpack.HotModuleReplacementPlugin(),
+    ],
+    resolve: {
+        extensions: ['*', '.js', '.jsx']
+    },
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        publicPath: '/',
+        filename: 'bundle.js'
+    },
+    devServer: {
+        contentBase: './dist'
+    }
+};
